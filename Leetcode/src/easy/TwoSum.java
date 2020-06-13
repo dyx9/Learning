c38be2d4118f8190fb38f6543fdbadf90f8e82e8
Yixuan Dai <747736317@qq.com>
2020-06-13 18:12:52 +0100

    fix Two Sum

--- Leetcode/src/easy/TwoSum.java
+++ Leetcode/src/easy/TwoSum.java
@@ -11,8 +11,7 @@ public class TwoSum {
         int [] result = new int[2];
         for (int i = 0; i < nums.length; i++) {
             for (int j = i+1; j < nums.length; j++) {
-                if (nums[i] == nums[j]) j++;
-                else if (nums[i] + nums[j] == target) {
+                if (nums[i] + nums[j] == target) {
                     result[0] = i;
                     result[1] = j;
                 }
