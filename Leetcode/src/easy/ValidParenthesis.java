9d6523d0f61f2da5a2f359ebd36cc414e06cc4df
Yixuan Dai <747736317@qq.com>
2020-06-19 18:05:40 +0100

    Valid Parenthesis

---
+++ Leetcode/src/easy/ValidParenthesis.java
@@ -0,0 +1,23 @@
+package easy;
+
+import java.util.Stack;
+
+public class ValidParenthesis {
+
+    public static boolean isValidParenthesis(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c:s.toCharArray()) {
+            if (c == '(') stack.push(')');
+            else if (c == '{') stack.push('}');
+            else if (c == '[') stack.push(']');
+            else if (stack.isEmpty() || c != stack.pop()) return false;
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValidParenthesis("()(){[]}"));
+        System.out.println(isValidParenthesis("{{[]}}"));
+        System.out.println(isValidParenthesis("({)}"));
+    }
+}
