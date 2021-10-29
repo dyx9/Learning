b9c06b65622bb4caad0ed3bf94891ae303f7277a
Yixuan Dai <747736317@qq.com>
2021-10-29 15:58:46 +0800

    StrStr

---
+++ Leetcode/src/easy/StrStr.java
@@ -0,0 +1,26 @@
+package easy;
+
+// https://leetcode-cn.com/problems/implement-strstr/
+
+public class StrStr {
+    public static int strStr(String haystack, String needle) {
+        int n = haystack.length(), m = needle.length();
+        for (int i = 0; i + m <= n; i++) {
+            boolean flag = true;
+            for (int j = 0; j < m; j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(strStr("hello", "ll"));
+    }
+}
