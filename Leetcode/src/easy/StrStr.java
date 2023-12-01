042de8b5a0d3fc7aa790ce57e2dcfc59f5a06969
Yixuan Dai <747736317@qq.com>
2023-12-01 20:41:08 +0000

    First Occurance of a String indexOf()
    brute force

--- Leetcode/src/easy/StrStr.java
+++ Leetcode/src/easy/StrStr.java
@@ -3,20 +3,33 @@ package easy;
 // https://leetcode-cn.com/problems/implement-strstr/
 
 public class StrStr {
+    // brute force, O(m*n)
     public static int strStr(String haystack, String needle) {
-        int n = haystack.length(), m = needle.length();
-        for (int i = 0; i + m <= n; i++) {
-            boolean flag = true;
-            for (int j = 0; j < m; j++) {
-                if (haystack.charAt(i + j) != needle.charAt(j)) {
-                    flag = false;
-                    break;
-                }
+        // go through each char of a given string
+        // i + substing len should <= string len, to avoid unnecessary check or Out-of-Bounds in 'while' 
+        for (int i = 0; i + needle.length() <= haystack.length(); i++) {
+            
+            // set another var stores i
+            // because directly modifing i in 'while' can also alter i in 'for'
+            int start = i;
+            // set a pointer to go through substring
+            int needlePtr = 0;
+
+            // if char matches
+            while (haystack.charAt(start) == needle.charAt(needlePtr)) {
+                start++;
+                needlePtr++;
+                // break if reach the end of substring
+                if (needlePtr >= needle.length()) break;
             }
-            if (flag) {
-                return i;
-            }
+            // if the substring has an occouance, return the start index
+            if (needlePtr == needle.length()) return i;
         }
+        return -1;
+    }
+
+    // KMP Algorithm O(m+n)
+    public static int strStrKMP(String haystack, String needle) {
         return -1;
     }
 
