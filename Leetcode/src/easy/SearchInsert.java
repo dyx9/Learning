a9e246c424a5f658cc233bc6361de73b245242dd
Yixuan Dai <747736317@qq.com>
2023-12-08 13:32:56 +0000

    Search Insert Revisit

--- Leetcode/src/easy/SearchInsert.java
+++ Leetcode/src/easy/SearchInsert.java
@@ -6,19 +6,12 @@ package easy;
 public class SearchInsert {
 
     public static int searchInsert(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
+        int left = 0, right = nums.length - 1;
         while (left <= right) {
-            int mid = (left + right);
-            if (target < nums[mid]) {
-                right = mid - 1;
-            }
-            else if (target > nums[mid]) {
-                left = mid + 1;
-            }
-            else {
-                return mid;
-            }
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] < target) left = mid + 1;
+            else right = mid - 1;
         }
         return left;
     }
