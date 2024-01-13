74818627a9376e336a5539bdda0493520cf8227a
Yixuan Dai <747736317@qq.com>
2024-01-13 22:33:46 +0000

    Chapter 1 - S1

---
+++ SwiftUITutorial/SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by Yixuan Dai on 13/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
