70899c05f2dc25dc8eed9daf6a3190cb92c30f21
Yixuan Dai <747736317@qq.com>
2024-01-14 00:01:20 +0000

    Chapter 1 - S2

---
+++ SwiftUITutorial/SwiftUITutorial/View/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  SwiftUITutorial
+//
+//  Created by Yixuan Dai on 13/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUITutorial/SwiftUITutorial/View/LandmarkList.swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkList.swift
+//  SwiftUITutorial
+//
+//  Created by Yixuan Dai on 13/01/2024.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationSplitView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        } detail: {
+            Text("Select a Landmark")
+        }
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
