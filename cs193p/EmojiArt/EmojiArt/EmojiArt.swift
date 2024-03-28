a03957fac91ed131650e95ac9ac4dd1e8212deae
Yixuan Dai <747736317@qq.com>
2024-03-28 16:52:04 +0000

    L10

--- cs193p/EmojiArt/EmojiArt/EmojiArt.swift
+++ cs193p/EmojiArt/EmojiArt/EmojiArt.swift
@@ -9,16 +9,26 @@ import Foundation
 
 struct EmojiArt {
     var background: URL?
-    var emojis = [Emoji]()
+    private(set) var emojis = [Emoji]()
     
-    struct Emoji {
+    private var uniqueEmojiID = 0
+    
+    mutating func addEmoji(_ emoji: String, at position: Emoji.Position, size: Int) {
+        uniqueEmojiID += 1
+        emojis.append(Emoji(string: emoji, position: position, size: size, id: uniqueEmojiID))
+    }
+    
+    struct Emoji: Identifiable {
         let string: String
         var position: Position
         var size: Int
+        var id: Int
         
         struct Position {
             var x: Int
             var y: Int
+            
+            static let zero = Position(x:0, y:0)
         }
     }
 }
--- cs193p/EmojiArt/EmojiArt/EmojiArtDocument.swift
+++ cs193p/EmojiArt/EmojiArt/EmojiArtDocument.swift
@@ -10,7 +10,12 @@ import SwiftUI
 class EmojiArtDocument: ObservableObject {
     typealias Emoji = EmojiArt.Emoji
     
-    private var emojiArt = EmojiArt()
+    @Published private var emojiArt = EmojiArt()
+    
+    init() {
+        emojiArt.addEmoji("🐮", at: .init(x: -200, y: 150), size: 200)
+        emojiArt.addEmoji("🥰", at: .init(x: 250, y: 100), size: 80)
+    }
     
     var emojis: [Emoji] {
         emojiArt.emojis
@@ -23,6 +28,23 @@ class EmojiArtDocument: ObservableObject {
     // MARK: - Intent(s)
     
     func setBackground(_ url: URL?) {
-         
+        emojiArt.background = url
+    }
+    
+    func addEmoji(_ emoji: String, at position: Emoji.Position, size: Int) {
+        emojiArt.addEmoji(emoji, at: position, size: size)
+    }
+}
+
+extension EmojiArt.Emoji {
+    var font: Font {
+        Font.system(size: CGFloat(size))
+    }
+}
+
+extension EmojiArt.Emoji.Position {
+    func `in`(_ geometry: GeometryProxy) -> CGPoint {
+        let center = geometry.frame(in: .local).center
+        return CGPoint(x: center.x + CGFloat(x), y: center.y - CGFloat(y))
     }
 }
